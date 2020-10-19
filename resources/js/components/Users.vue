0b2aa2ec052ac213c176ed831be04509250cef73
sachithdm <sachithdm@gmail.com>
2020-10-19 20:57:17 +0530

    Using VFrom  Backend Error Handl in Laravel & Vuej

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -7,7 +7,11 @@
             <h3 class="card-title">Users Table</h3>
 
             <div class="card-tools">
-              <button class="btn btn-success" data-toggle="modal" data-target="#addNew">
+              <button
+                class="btn btn-success"
+                data-toggle="modal"
+                data-target="#addNew"
+              >
                 Add New <i class="fas fa-user-plus fa-fw"></i>
               </button>
             </div>
@@ -58,7 +62,7 @@
       aria-labelledby="addNewLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog modal-dialog-centered" role="document" >
+      <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="addNewLabel">Add New</h5>
@@ -71,13 +75,50 @@
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">...</div>
+          
+          <div class="modal-body">
+            
+            <div class="form-group">
+              <input v-model="form.name" type="text" name="name" placeholder="Name" class="form-control"
+                :class="{ 'is-invalid': form.errors.has('name') }" />
+              <has-error :form="form" field="name"></has-error>
+            </div>
+
+            <div class="form-group">
+              <input v-model="form.email" type="email" name="email" placeholder="Email Address" class="form-control"
+                :class="{ 'is-invalid': form.errors.has('email') }" />
+              <has-error :form="form" field="email"></has-error>
+            </div>
+
+            <div class="form-group">
+              <textarea v-model="form.bio" type="text" name="bio" id="bio" placeholder="Short bio for user (optional)"
+               class="form-control"
+                :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+              <has-error :form="form" field="bio"></has-error>
+            </div>
+
+            <div class="form-group">
+              <select name="type" v-model="form.type" id="type" class="form-control" 
+              :class="{ 'is-invalid': form.errors.has('type') }">
+              <option value="">Select User Role</option>
+              <option value="admin">Admin</option>
+              <option value="user">Standard User</option>
+              <option value="author">Author</option>
+              <has-error :form="form" field="type"></has-error>            
+              </select>              
+            </div>
+
+            <div class="form-group">
+              <input v-model="form.password" type="password" name="password" id="password" 
+               class="form-control"
+                :class="{ 'is-invalid': form.errors.has('password') }" />
+              <has-error :form="form" field="password"></has-error>
+            </div>
+
+          </div>
+
           <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-danger"
-              data-dismiss="modal"
-            >
+            <button type="button" class="btn btn-danger" data-dismiss="modal">
               Close
             </button>
             <button type="button" class="btn btn-primary">Create</button>
@@ -85,13 +126,23 @@
         </div>
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      form: new Form({
+        name: "",
+        email: "",
+        password: "",
+        type: "",
+        bio: "",
+        photo: "",
+      }),
+    };
+  },
   mounted() {
     console.log("Component mounted.");
   },
