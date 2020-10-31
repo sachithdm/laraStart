b798d3232c2f69ef187dc05ef8c4cd313270db28
sachithdm <sachithdm@gmail.com>
2020-10-31 19:57:10 +0530

    Laravel 5.7 and Vuejs Ajax Pagination

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -3,7 +3,6 @@
     <div class="row mt-5" v-if="$gate.isAdminOrAuthor()">
       <div class="col-md-12">
         <div class="card">
-
           <div class="card-header">
             <!-- Table Heading -->
             <h3 class="card-title">Users Table</h3>
@@ -12,13 +11,13 @@
               <button class="btn btn-success" @click="newModal">
                 Add New <i class="fas fa-user-plus fa-fw"></i>
               </button>
-              
 
               <!-- <button class="btn btn-success" data-toggle="modal" data-target="#addNew">
                 Add New <i class="fas fa-user-plus fa-fw"></i>
               </button> -->
             </div>
-          </div><!-- /.card-header -->
+          </div>
+          <!-- /.card-header -->
 
           <div class="card-body table-responsive p-0">
             <table class="table table-hover text-nowrap">
@@ -33,7 +32,7 @@
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="user in users" :key="user.id">
+                <tr v-for="user in users.data" :key="user.id">
                   <td>{{ user.id }}</td>
                   <td>{{ user.name }}</td>
                   <td>{{ user.email }}</td>
@@ -51,8 +50,16 @@
                 </tr>
               </tbody>
             </table>
-          </div><!-- /.card-body -->
-        </div><!-- /.card -->
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer">
+            <pagination
+              :data="users"
+              @pagination-change-page="getResults"
+            ></pagination>
+          </div>
+        </div>
+        <!-- /.card -->
       </div>
     </div>
 
@@ -60,17 +67,30 @@
       <not-found></not-found>
     </div>
 
-
-
     <!-- Modal to add and Edit Users -->
-    <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel"
-      aria-hidden="true">
+    <div
+      class="modal fade"
+      id="addNew"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="addNewLabel"
+      aria-hidden="true"
+    >
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 v-show="editmode" class="modal-title" id="addNewLabel">Update User's Information</h5>
-            <h5 v-show="!editmode" class="modal-title" id="addNewLabel">Add New</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <h5 v-show="editmode" class="modal-title" id="addNewLabel">
+              Update User's Information
+            </h5>
+            <h5 v-show="!editmode" class="modal-title" id="addNewLabel">
+              Add New
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
@@ -79,30 +99,52 @@
           <form @submit.prevent="editmode ? updateUser() : createUser()">
             <!-- @keydown="form.onKeydown($event)" -->
             <div class="modal-body">
-              
               <div class="form-group">
-                <input v-model="form.name" type="text" name="name" placeholder="Name" class="form-control"
-                  :class="{ 'is-invalid': form.errors.has('name') }"/>
+                <input
+                  v-model="form.name"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('name') }"
+                />
                 <has-error :form="form" field="name"></has-error>
               </div>
 
               <div class="form-group">
-                <input v-model="form.email" type="email" name="email" placeholder="Email Address"
-                  class="form-control" :class="{ 'is-invalid': form.errors.has('email') }"/>
+                <input
+                  v-model="form.email"
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('email') }"
+                />
                 <has-error :form="form" field="email"></has-error>
               </div>
 
               <div class="form-group">
-                <textarea v-model="form.bio" type="text" name="bio" id="bio" 
-                placeholder="Short bio for user (optional)" class="form-control"
-                  :class="{ 'is-invalid': form.errors.has('bio') }" >
+                <textarea
+                  v-model="form.bio"
+                  type="text"
+                  name="bio"
+                  id="bio"
+                  placeholder="Short bio for user (optional)"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('bio') }"
+                >
                 </textarea>
                 <has-error :form="form" field="bio"></has-error>
               </div>
 
               <div class="form-group">
-                <select name="type" v-model="form.type" id="type" class="form-control" 
-                :class="{ 'is-invalid': form.errors.has('type') }" >
+                <select
+                  name="type"
+                  v-model="form.type"
+                  id="type"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('type') }"
+                >
                   <option value="">Select User Role</option>
                   <option value="admin">Admin</option>
                   <option value="user">Standard User</option>
@@ -112,8 +154,14 @@
               </div>
 
               <div class="form-group">
-                <input v-model="form.password" type="password" name="password" id="password" 
-                class="form-control" :class="{ 'is-invalid': form.errors.has('password') }" />
+                <input
+                  v-model="form.password"
+                  type="password"
+                  name="password"
+                  id="password"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('password') }"
+                />
                 <has-error :form="form" field="password"></has-error>
               </div>
             </div>
@@ -122,8 +170,12 @@
               <button type="button" class="btn btn-danger" data-dismiss="modal">
                 Close
               </button>
-              <button v-show="editmode" type="submit" class="btn btn-success">Update</button>
-              <button v-show="!editmode" type="submit" class="btn btn-primary">Create</button>
+              <button v-show="editmode" type="submit" class="btn btn-success">
+                Update
+              </button>
+              <button v-show="!editmode" type="submit" class="btn btn-primary">
+                Create
+              </button>
             </div>
           </form>
         </div>
@@ -136,10 +188,10 @@
 export default {
   data() {
     return {
-      editmode:false,
-      users: {},
+      editmode: false,
+      users : {},
       form: new Form({
-        id:'',
+        id: "",
         name: "",
         email: "",
         password: "",
@@ -150,43 +202,50 @@ export default {
     };
   },
   methods: {
-    updateUser(){
+    getResults(page = 1) {
+      axios.get('api/user?page=' + page)
+      .then(response => {
+        this.users = response.data;
+      });
+    },
+    updateUser() {
       //Start Progress Bar
       this.$Progress.start();
       //console.log('Editing data');
-      this.form.put('api/user/'+this.form.id)
-      .then(() => {
-        //success
-        //Hide Add New User Modal
+      this.form
+        .put("api/user/" + this.form.id)
+        .then(() => {
+          //success
+          //Hide Add New User Modal
           $("#addNew").modal("hide");
-        swal.fire("Updated!", "Information has been updated.", "success");
-        //End Progress Bar
-        this.$Progress.finish();        
-        //refresh table after update
-        Fire.$emit("AfterCreate");
-      }) 
-      .catch(() => {
-        //errors
-        //fail Progress Bar
-        this.$Progress.fail();
-      });
+          swal.fire("Updated!", "Information has been updated.", "success");
+          //End Progress Bar
+          this.$Progress.finish();
+          //refresh table after update
+          Fire.$emit("AfterCreate");
+        })
+        .catch(() => {
+          //errors
+          //fail Progress Bar
+          this.$Progress.fail();
+        });
     },
-    editModal(user){
+    editModal(user) {
       //set edit mode true
       this.editmode = true;
       //reset the form
       this.form.reset();
-      //Show Add New User Modal      
+      //Show Add New User Modal
       $("#addNew").modal("show");
       //fill the input fileds using user data to edit
       this.form.fill(user);
     },
-    newModal(){
+    newModal() {
       //set edit mode false
       this.editmode = false;
       //reset the form
       this.form.reset();
-      //Show Add New User Modal      
+      //Show Add New User Modal
       $("#addNew").modal("show");
     },
     deleteUser(id) {
@@ -204,7 +263,9 @@ export default {
         .then((result) => {
           //Send request to the server to delete
           if (result.isConfirmed) {
-            this.form.delete("/api/user/" + id).then(() => {
+            this.form
+              .delete("/api/user/" + id)
+              .then(() => {
                 swal.fire("Deleted!", "Your file has been deleted.", "success");
                 //refresh table after delete
                 Fire.$emit("AfterCreate");
@@ -213,13 +274,12 @@ export default {
                 swal.fire("Failed!", "THere was something wrong.", "warning");
               });
           }
-        })
+        });
     },
     loadUsers() {
-      if(this.$gate.isAdminOrAuthor()){
-        axios.get("api/user").then(({ data }) => (this.users = data.data));
+      if (this.$gate.isAdminOrAuthor()) {
+        axios.get("api/user").then(({ data }) => (this.users = data));
       }
-      
     },
     createUser() {
       //Start Progress Bar
